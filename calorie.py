b47245b3fc95d978b014fc981f0ea088efca5f1e
shihabdelwar <shihabdelwar@gmail.com>
2025-01-15 13:04:07 -0500

    Shihab Delwar

--- calorie.py
+++ calorie.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template_string, send_from_directory, redirect
+from flask import Flask, request, render_template, render_template_string, send_from_directory, redirect
 import os
 
 app = Flask(__name__)
